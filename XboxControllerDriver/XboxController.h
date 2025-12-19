eee12edcfee00bff5d6d9f75dd0fb687362a8a66
Jonas Wander <53247355+jonasw8@users.noreply.github.com>
2025-12-19 17:49:44 -0300

    Update XboxController.h

--- XboxControllerDriver/XboxController.h
+++ XboxControllerDriver/XboxController.h
@@ -1,50 +1,29 @@
 #pragma once
 
 #include <DriverKit/DriverKit.h>
-#include <USBDriverKit/USBDriverKit.h>
-#include <HIDDriverKit/HIDDriverKit.h>
+#include <HIDDriverKit/IOHIDDevice.h>
+#include <DriverKit/IOLib.h>
 #include <DriverKit/IOBufferMemoryDescriptor.h>
 
-struct XboxControllerState {
-    bool buttonA, buttonB, buttonX, buttonY;
-    bool dpadUp, dpadDown, dpadLeft, dpadRight;
-    int16_t leftStickX, leftStickY;
-};
+using namespace std;
 
-class XboxController : public IOUserHIDDevice {
-    OSDeclareDefaultStructors(XboxController)
+class XboxController : public IOHIDDevice
+{
+    DKDeclareDefaultStructors(XboxController)
 
 public:
     // Lifecycle
-    bool init() override;
-    kern_return_t Start(IOService* provider) override;
-    kern_return_t Stop(IOService* provider) override;
-    void free() override;
+    virtual bool init() override;
+    virtual kern_return_t Start(IOService *provider) override;
+    virtual kern_return_t Stop(IOService *provider) override;
+    virtual void free() override;
 
-    // HID
-    OSDictionary* newDeviceDescription() override;
-    OSData* newReportDescriptor() override;
-    void UpdateHIDReport(const XboxControllerState& state);
+    // HID reporting
+    virtual OSData* newReportDescriptor() override;
+    virtual OSDictionary* newDeviceDescription() override;
 
-    // USB
-    kern_return_t SetupUSB();
-    kern_return_t StartReading();
-    kern_return_t SendInitCommand();
-
-    // Callback
-    kern_return_t HandleReadComplete(
-        OSAction* action,
-        IOReturn status,
-        uint32_t bytesTransferred
-    );
+    kern_return_t UpdateButtons(uint16_t mask);
 
 private:
-    IOUSBHostInterface* usbInterface = nullptr;
-    IOUSBHostPipe* inputPipe = nullptr;
-    IOUSBHostPipe* outputPipe = nullptr;
-
-    IOBufferMemoryDescriptor* inputBuffer = nullptr;
-    IOBufferMemoryDescriptor* hidBuffer = nullptr;
-
-    OSAction* readAction = nullptr;
+    IOBufferMemoryDescriptor *reportBuffer = nullptr;
 };
